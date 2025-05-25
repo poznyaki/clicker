b496231979646dfd989855527f23e144260eff21
Snizhana <snezha.uspih@gemail.come>
2025-05-25 19:53:00 +0300

    Print

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import sys
+
 print("Hello world")
-print("Hi")
+from PyQt5.QtWidgets import *
+
+class MyApp(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.init__ui()
+        self.show()
+
+    def init_ui(self):
+        self.button.clicked.connect(self.on_click)
+        self.hello_button.clicked.connect(self.hello)
+        self.love_button.clicked.connect(self.love)
+        self.eval_button.clicked.connect(self.eval_click)
+        self.setLayout(self.layout)
+
+
+
+
+
+
+app = QApplication(sys.argv)
+window = MyApp()
+window.show()
+sys.exit(app.exec_())
